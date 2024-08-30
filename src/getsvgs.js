c73fb61df411c466ed7a2038a572068d10790081
Juan <3447894+riosjuan@users.noreply.github.com>
2024-08-30 09:31:34 +0200

    Fixed issue with copy to clipboard notification and added second download-all button

--- src/getsvgs.js
+++ src/getsvgs.js
@@ -3,42 +3,65 @@ import { saveAs } from 'file-saver';
 
 // Function to set SVGs and update the page content
 const setSvgsUrl = (data, sender, pageUrl) => {
-  document.getElementById(
-    'header'
-  ).innerHTML = `All SVGs from &#8594; ${sender}`;
+  const header = document.getElementById('header');
   const senderLink = document.getElementById('sender-url');
-  senderLink.innerHTML = pageUrl.replace(/\/$/, '');
-  senderLink.href = pageUrl;
-
   const svgContainer = document.getElementById('svgcard');
-  svgContainer.innerHTML = ''; // Clear any existing SVG cards
+  const downloadAllButtons = document.querySelectorAll('.button-download-all');
+  const disclaimer = document.querySelector('.disclaimer');
+
+  if (header) {
+    header.innerHTML = `All SVGs from &#8594; ${sender}`;
+  }
+
+  if (senderLink) {
+    senderLink.innerHTML = pageUrl.replace(/\/$/, '');
+    senderLink.href = pageUrl;
+  }
+
+  if (svgContainer) {
+    svgContainer.innerHTML = ''; // Clear any existing SVG cards
+  }
 
   if (data.length === 0) {
-    document.getElementById(
-      'header'
-    ).innerHTML = `No SVGs in &#8594; ${sender}`;
-    document.getElementById('download-all').classList.add('hidden');
-    const disclaimer = document.getElementsByClassName('disclaimer')[0];
-    disclaimer.innerHTML = 'It seems that this site does not use any SVGs. 🙃';
+    if (header) {
+      header.innerHTML = `No SVGs in &#8594; ${sender}`;
+    }
+    downloadAllButtons.forEach((button) => {
+      button.classList.add('hidden');
+    });
+    if (disclaimer) {
+      disclaimer.innerHTML =
+        'It seems that this site does not use any SVGs. 🙃';
+    }
   } else {
     data.forEach((svg, index) => {
-      const element = document.createElement('div');
-      const base64doc = btoa(unescape(encodeURIComponent(svg)));
-      element.innerHTML = `
-        <div>
-          ${svg}
-        </div>
-        <div class="actions">
-          <button class='copy'>Copy</button>
-          <a download="${sender}-${index}.svg" href='data:text/svg;base64,${base64doc}'>Download</a>
-        </div>
-      `;
-      element.classList.add('svg-card');
-      svgContainer.appendChild(element);
+      if (svgContainer) {
+        const element = document.createElement('div');
+        const base64doc = btoa(unescape(encodeURIComponent(svg)));
+        element.innerHTML = `
+          <div>
+            ${svg}
+          </div>
+          <div class="actions">
+            <button class='copy'>Copy</button>
+            <a download="${sender}-${index}.svg" href='data:text/svg;base64,${base64doc}'>Download</a>
+          </div>
+        `;
+        element.classList.add('svg-card');
+        svgContainer.appendChild(element);
+
+        // Add event listener for copy button
+        const copyButton = element.querySelector('button.copy');
+        if (copyButton) {
+          copyButton.addEventListener('click', copySvg);
+        }
+      }
     });
 
-    // Show the 'Download All' button if SVGs are present
-    document.getElementById('download-all').classList.remove('hidden');
+    // Show the 'Download All' buttons if SVGs are present
+    downloadAllButtons.forEach((button) => {
+      button.classList.remove('hidden');
+    });
   }
 
   // Add event listeners for copy buttons
@@ -50,33 +73,45 @@ const setSvgsUrl = (data, sender, pageUrl) => {
 // Function to copy SVG to clipboard
 const copySvg = (event) => {
   const notification = document.querySelector('.notification');
-  const svg = event.target.closest('.svg-card').querySelector('svg');
+  const svgCard = event.target.closest('.svg-card');
 
-  navigator.clipboard
-    .writeText(svg.outerHTML)
-    .then(() => {
-      notification.classList.remove('notification-off');
-      notification.classList.add('notification-on');
+  if (!svgCard) {
+    console.error('SVG card not found');
+    return;
+  }
 
-      setTimeout(() => {
-        notification.classList.remove('notification-on');
-        notification.classList.add('notification-off');
-      }, 1500);
-    })
-    .catch((err) => {
-      console.error('Failed to copy text: ', err);
-    });
+  const svg = svgCard.querySelector('svg');
+
+  if (svg) {
+    navigator.clipboard
+      .writeText(svg.outerHTML)
+      .then(() => {
+        if (notification) {
+          notification.classList.add('notification-on');
+          setTimeout(() => {
+            notification.classList.remove('notification-on');
+          }, 1500);
+        }
+      })
+      .catch((err) => {
+        console.error('Failed to copy text: ', err);
+      });
+  } else {
+    console.error('SVG element not found');
+  }
 };
 
 // Set up 'Download All' button functionality
-document.getElementById('download-all').addEventListener('click', () => {
-  const zip = new JSZip();
-  document.querySelectorAll('.svg-card svg').forEach((svg, index) => {
-    zip.file(`svg${index}.svg`, svg.outerHTML);
-  });
+document.querySelectorAll('.button-download-all').forEach((button) => {
+  button.addEventListener('click', () => {
+    const zip = new JSZip();
+    document.querySelectorAll('.svg-card svg').forEach((svg, index) => {
+      zip.file(`svg${index}.svg`, svg.outerHTML);
+    });
 
-  zip.generateAsync({ type: 'blob' }).then((content) => {
-    saveAs(content, 'svgs_collection.zip');
+    zip.generateAsync({ type: 'blob' }).then((content) => {
+      saveAs(content, 'svgs_collection.zip');
+    });
   });
 });
 
@@ -86,3 +121,23 @@ chrome.runtime.onMessage.addListener((message) => {
     setSvgsUrl(message.data, message.sender, message.pageUrl);
   }
 });
+
+//TODO: move to another file
+const header = document.querySelector('.header');
+const downloadAllButtons = document.querySelectorAll('.button-download-all');
+
+if (header && downloadAllButtons.length >= 2) {
+  const headerHeight = header.offsetHeight;
+  const fixedButton = downloadAllButtons[0];
+  const originalButton = downloadAllButtons[1];
+
+  window.addEventListener('scroll', () => {
+    const originalButtonRect = originalButton.getBoundingClientRect();
+
+    if (originalButtonRect.top < headerHeight) {
+      fixedButton.classList.remove('button-download-all-hidden');
+    } else {
+      fixedButton.classList.add('button-download-all-hidden');
+    }
+  });
+}
